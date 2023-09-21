5ea3dab97dba2dbce88612bc3678d85c1341338a
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-09-21 19:57:11 +0700

    finishing orange diseases classification

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import os
 from flask import Flask
 
 from index import bp as index_bp
+from orange_diseases import bp as orange_bp
 
 app = Flask(__name__)
 
 
 app.register_blueprint(index_bp)
+app.register_blueprint(orange_bp)
 
 
 if __name__ == "__main__":
