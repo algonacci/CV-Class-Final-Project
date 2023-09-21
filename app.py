3a46f53830a1743bf7bd587983af08fe4c744259
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-09-21 19:04:03 +0700

    fixing copywriting and folder structure

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask
+
+from index import bp as index_bp
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+app.register_blueprint(index_bp)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True,
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8080)))
